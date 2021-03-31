f1d6fd9389ced9104ee4a92eb33333c707c93a86
SoraYama <sorayamahou@gmail.com>
2021-03-31 20:59:46 +0800

    fix: remove agent on prod env

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,19 @@ global.APP_PATH = __dirname;
 import path from 'path';
 import Octo from '@octo-bot/core';
 import TomonBot from '@octo-bot/tomon-bot';
-// import DiscordBot from '@octo-bot/discord-bot';
+import DiscordBot from '@octo-bot/discord-bot';
 import TelegramBot from '@octo-bot/telegram-bot';
-import SocksProxyAgent from 'socks-proxy-agent';
-
-const agent = SocksProxyAgent('socks://127.0.0.1:1080');
-
-const tomonOptions = {
-  // axiosConfig: {
-  //   httpsAgent: agent,
-  // },
-  wsOptions: {
-    agent,
-  },
-};
 
 const ROOT = path.resolve(__dirname);
 
-const tomonBot = new TomonBot(ROOT, 'tomon', tomonOptions);
+const tomonBot = new TomonBot(ROOT, 'tomon');
 const telegramBot = new TelegramBot(ROOT, 'telegram', {
-  telegram: {
-    agent,
-  },
   handlerTimeout: 10000,
 });
-// const discordBot = new DiscordBot(ROOT, 'discord');
+const discordBot = new DiscordBot(ROOT, 'discord');
 
 const instance = Octo.getInstance({
-  // bots: [tomonBot, telegramBot, discordBot],
-  bots: [tomonBot, telegramBot],
-  // bots: [telegramBot],
+  bots: [tomonBot, telegramBot, discordBot],
   ROOT,
 });
 
