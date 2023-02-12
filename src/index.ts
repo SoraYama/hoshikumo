94fdddcafda3e62a8976bbdf0c2fcf2b61685e8e
SoraYama <sorayamahou@gmail.com>
2023-02-12 17:18:50 +0800

    chore: bump deps

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
-global.APP_PATH = __dirname;
-
 import path from 'path';
+
 import Octo from '@octo-bot/core';
-import TomonBot from '@octo-bot/tomon-bot';
-import DiscordBot from '@octo-bot/discord-bot';
+// import TomonBot from '@octo-bot/tomon-bot';
+// import DiscordBot from '@octo-bot/discord-bot';
 import TelegramBot from '@octo-bot/telegram-bot';
+
+global.APP_PATH = __dirname;
 
 const ROOT = path.resolve(__dirname);
 
-const tomonBot = new TomonBot(ROOT, 'tomon');
+// const tomonBot = new TomonBot(ROOT, 'tomon');
+// const telegramBot = new TelegramBot(ROOT, 'telegram', {
+//   handlerTimeout: 10000,
+// });
+// const discordBot = new DiscordBot(ROOT, 'discord');
+
+// const instance = Octo.getInstance({
+//   bots: [tomonBot, telegramBot, discordBot],
+//   ROOT,
+// });
+
 const telegramBot = new TelegramBot(ROOT, 'telegram', {
   handlerTimeout: 10000,
 });
-const discordBot = new DiscordBot(ROOT, 'discord');
-
 const instance = Octo.getInstance({
-  bots: [tomonBot, telegramBot, discordBot],
+  bots: [telegramBot],
   ROOT,
 });
 
