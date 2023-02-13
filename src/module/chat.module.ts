1ff8cb69b0730500e415523e15245a3a9d2ee8c8
SoraYama <sorayamahou@gmail.com>
2023-02-13 09:54:51 +0800

    fix: catch reply error

--- src/module/chat.module.ts
+++ src/module/chat.module.ts
@@ -15,28 +15,36 @@ const openAI = new OpenAIApi(openAIConfig);
 class ChatGPTModule extends BaseModule {
   @Trigger({ match: 'ask', methods: [TriggerMethod.Prefix] })
   public async ask() {
-    const prompt = this.event.params[0];
-    this.bot.logger.info(`prompt - ${prompt}`);
+    try {
+      const prompt = this.event.params[0];
+      this.bot.logger.info(`prompt - ${prompt}`);
 
-    const response = await openAI.createCompletion({
-      model: 'text-davinci-003',
-      prompt,
-      temperature: 0.9, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）
-      max_tokens: 4000,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.6,
-      stop: [' Human:', ' AI:'],
-    });
+      const response = await openAI.createCompletion({
+        model: 'text-davinci-003',
+        prompt,
+        temperature: 0.9, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）
+        max_tokens: 4000,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.6,
+        stop: [' Human:', ' AI:'],
+      });
 
-    const content = remark()
-      .use(stripMarkdown)
-      .processSync(response.data.choices?.[0].text ?? '')
-      .toString();
+      const content = remark()
+        .use(stripMarkdown)
+        .processSync(response.data.choices?.[0].text ?? '')
+        .toString();
 
-    this.event.reply({
-      content,
-    });
+      this.event.reply({
+        content,
+      });
+    } catch (e) {
+      this.bot.logger.error(e);
+
+      this.event.reply({
+        content: 'oops, 出错了',
+      });
+    }
   }
 }
 
