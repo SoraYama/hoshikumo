ef63d070c1e11a2f589a74684d3dc3974fd2dff3
SoraYama <sorayamahou@gmail.com>
2024-10-09 10:20:45 +0800

    feat: add qq bot

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,35 @@
 global.APP_PATH = __dirname;
 
+import dotenv from 'dotenv';
 import path from 'path';
 import Octo from '@octo-bot/core';
-import TomonBot from '@octo-bot/tomon-bot';
-// import DiscordBot from '@octo-bot/discord-bot';
-import TelegramBot from '@octo-bot/telegram-bot';
-import SocksProxyAgent from 'socks-proxy-agent';
+import QQBot from '@octo-bot/qq-bot';
+import { AvailableIntentsEventsEnum } from 'qq-guild-bot';
 
-const agent = SocksProxyAgent('socks://127.0.0.1:1080');
-
-const tomonOptions = {
-  // axiosConfig: {
-  //   httpsAgent: agent,
-  // },
-  wsOptions: {
-    agent,
-  },
-};
+const { REDIS_HOST, REDIS_PORT, REDIS_DB, QQ_BOT_TOKEN, QQ_APP_ID } = dotenv.config().parsed || {};
 
 const ROOT = path.resolve(__dirname);
 
-const tomonBot = new TomonBot(ROOT, 'tomon', tomonOptions);
-const telegramBot = new TelegramBot(ROOT, 'telegram', {
-  telegram: {
-    agent,
-  },
-  handlerTimeout: 10000,
+const qqBot = new QQBot(ROOT, 'qq', {
+  appID: QQ_APP_ID,
+  token: QQ_BOT_TOKEN,
+  intents: [
+    AvailableIntentsEventsEnum.PUBLIC_GUILD_MESSAGES,
+    AvailableIntentsEventsEnum.DIRECT_MESSAGE,
+    AvailableIntentsEventsEnum.INTERACTION,
+  ],
+  sandbox: true,
 });
-// const discordBot = new DiscordBot(ROOT, 'discord');
 
 const instance = Octo.getInstance({
-  // bots: [tomonBot, telegramBot, discordBot],
-  bots: [tomonBot, telegramBot],
-  // bots: [telegramBot],
+  bots: [qqBot],
   ROOT,
+  redis: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    database: REDIS_DB,
+    // password: REDIS_PASSWORD,
+  },
 });
 
 instance.start();
